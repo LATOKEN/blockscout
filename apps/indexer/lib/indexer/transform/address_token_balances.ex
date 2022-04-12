a1ce1404b734035b99aba5357d34307fb51595aa
tbssajal <tariq.salam@latoken.com>
2022-04-12 23:55:56 +0600

    updated token-balance

--- apps/indexer/lib/indexer/transform/address_token_balances.ex
+++ apps/indexer/lib/indexer/transform/address_token_balances.ex
@@ -1,5 +1,3 @@
-require Logger
-
 defmodule Indexer.Transform.AddressTokenBalances do
   @moduledoc """
   Extracts `Explorer.Address.TokenBalance` params from other schema's params.
@@ -14,27 +12,28 @@ defmodule Indexer.Transform.AddressTokenBalances do
   defp reducer({:token_transfers_params, token_transfers_params}, initial) when is_list(token_transfers_params) do
     token_transfers_params
     |> ignore_burn_address_transfers_for_token_larc_721
-     |> Enum.reduce(initial, fn %{
+    |> Enum.reduce(initial, fn %{
                                  block_number: block_number,
                                  from_address_hash: from_address_hash,
                                  to_address_hash: to_address_hash,
                                  token_contract_address_hash: token_contract_address_hash,
+                                 token_id: token_id,
                                  token_type: token_type
-                               }= params,
+                               } = params,
                                acc
                                when is_integer(block_number) and is_binary(from_address_hash) and
                                       is_binary(to_address_hash) and is_binary(token_contract_address_hash) ->
-      if params[:token_ids] && token_type == "LRC-1155" do
+      if params[:token_ids] && token_type == "LARC-1155" do
         params[:token_ids]
-        |> Enum.reduce(acc, fn _, sub_acc ->
+        |> Enum.reduce(acc, fn id, sub_acc ->
           sub_acc
-          |> add_token_balance_address(from_address_hash, token_contract_address_hash, block_number, token_type)
-          |> add_token_balance_address(from_address_hash, token_contract_address_hash, block_number, token_type)
+          |> add_token_balance_address(from_address_hash, token_contract_address_hash, id, token_type, block_number)
+          |> add_token_balance_address(to_address_hash, token_contract_address_hash, id, token_type, block_number)
         end)
       else
         acc
-        |> add_token_balance_address(from_address_hash, token_contract_address_hash, block_number, token_type)
-        |> add_token_balance_address(from_address_hash, token_contract_address_hash, block_number, token_type)
+        |> add_token_balance_address(from_address_hash, token_contract_address_hash, token_id, token_type, block_number)
+        |> add_token_balance_address(to_address_hash, token_contract_address_hash, token_id, token_type, block_number)
       end
     end)
   end
@@ -43,13 +42,14 @@ defmodule Indexer.Transform.AddressTokenBalances do
     Enum.filter(token_transfers_params, &do_filter_burn_address/1)
   end
 
-  defp add_token_balance_address(map_set, unquote(@burn_address), _, _, _), do: map_set
+  defp add_token_balance_address(map_set, unquote(@burn_address), _, _, _, _), do: map_set
 
-  defp add_token_balance_address(map_set, address, token_contract_address, block_number, token_type) do
+  defp add_token_balance_address(map_set, address, token_contract_address, token_id, token_type, block_number) do
     MapSet.put(map_set, %{
       address_hash: address,
       token_contract_address_hash: token_contract_address,
       block_number: block_number,
+      token_id: token_id,
       token_type: token_type
     })
   end
