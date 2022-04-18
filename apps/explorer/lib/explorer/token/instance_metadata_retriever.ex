90827eb4fa731fc371bf8c109c963abf27789244
tbssajal <tariq.salam@latoken.com>
2022-04-18 16:23:38 +0600

    fixed format

--- apps/explorer/lib/explorer/token/instance_metadata_retriever.ex
+++ apps/explorer/lib/explorer/token/instance_metadata_retriever.ex
@@ -223,21 +223,21 @@ defmodule Explorer.Token.InstanceMetadataRetriever do
       end
     end
 
-    rescue
-      e ->
-        Logger.debug(["Could not send request to token uri #{inspect(uri)}. error #{inspect(e)}"],
-          fetcher: :token_instances
-        )
-        newUri = if String.contains?(uri, "gateway.pinata.cloud") do
-          String.replace(uri, "gateway.pinata.cloud", "latoken.mypinata.cloud")
-        else
-          uri
-        end
-        Logger.debug(["Could not send request to new token uri #{inspect(newUri)}. error #{inspect(e)}"],
-          fetcher: :token_instances
-        )
+  rescue
+    e ->
+      Logger.debug(["Could not send request to token uri #{inspect(uri)}. error #{inspect(e)}"],
+        fetcher: :token_instances
+      )
+      newUri = if String.contains?(uri, "gateway.pinata.cloud") do
+        String.replace(uri, "gateway.pinata.cloud", "latoken.mypinata.cloud")
+      else
+        uri
+      end
+      Logger.debug(["Could not send request to new token uri #{inspect(newUri)}. error #{inspect(e)}"],
+        fetcher: :token_instances
+      )
 
-        {:error, :request_error}
+      {:error, :request_error}
 
   end
 
