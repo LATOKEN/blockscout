7cfae3bf56a8322f98d1bbd98201a961d8e8f16b
tbssajal <tariq.salam@latoken.com>
2022-05-16 13:25:49 +0600

    changed network name

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -44,7 +44,7 @@ defmodule BlockScoutWeb.LayoutView do
   end
 
   def network_title do
-    Keyword.get(application_config(), :network) || "POA"
+    Keyword.get(application_config(), :network) || "Lachain"
   end
 
   defp application_config do
